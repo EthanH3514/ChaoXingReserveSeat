f96ef613101bc7c2121809ac9072313d1efe8a86
EthanH3514 <1326414961@qq.com>
2024-11-22 14:10:25 +0800

    debug

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,12 +1,13 @@
 import os 
 from .encrypt import AES_Encrypt, generate_captcha_key, enc
 from .reserve import reserve
+import logging
 
 def _fetch_env_variables(env_name, action):
     try:
         return os.environ[env_name] if action else ""
     except KeyError:
-        print(f"Environment variable {env_name} is not configured correctly.")
+        logging.info(f"Environment variable {env_name} is not configured correctly.")
         return None
 
 def get_user_credentials(action):
